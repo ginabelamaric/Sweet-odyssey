ffafb94bfda6901a8c8927599d1d25df15422f72
ginabelamaric <92459363+ginabelamaric@users.noreply.github.com>
2024-01-23 01:17:10 +0100

    page izmjenjen

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,21 +1,48 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom"; // Import Link from your routing library
 
 const Login: React.FC = () => {
-    return (
-        <div id="login_signup">
-            <h1>Log in</h1>
-            <form>
-                <label htmlFor="username">Username</label><br />
-                <input type="text" id="username" name="username" placeholder="Username" required /><br />
-                <label htmlFor="password">Password</label><br />
-                <input type="password" id="password" name="password" placeholder="Password" required /><br />
-                <label htmlFor="email">Email</label><br />
-                <input type="email" id="email" name="email" placeholder="Email" required /><br />
-                <button><a href="/">LOG IN</a></button>
-                <p>Don't have an account? <a href="/signup">Sign up.</a></p>
-            </form>
-        </div>
-    );
-}
+  return (
+    <div id="login_signup">
+      <h1>Log in</h1>
+      <form action="/login" method="post">
+        <label htmlFor="username">Username</label>
+        <br />
+        <input
+          type="text"
+          id="username"
+          name="username"
+          placeholder="Username"
+          required
+        />
+        <br />
+        <label htmlFor="password">Password</label>
+        <br />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Password"
+          required
+        />
+        <br />
+        <label htmlFor="email">Email</label>
+        <br />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          required
+        />
+        <br />
+        <button type="submit">LOG IN</button>
+        <p>
+          Don't have an account? <Link to="/signup">Sign up.</Link>
+        </p>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
